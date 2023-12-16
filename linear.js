1f660106119f2ec5775cdaa6fd4c6739835d7d0b
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-16 23:54:46 +0600

    Update linear.js

--- linear.js
+++ linear.js
@@ -1,1 +1,13 @@
+let numbers = [1,2,3,4,5,6,7,8,9];
 
+function linearSearch (arr,target) {
+  for(i=0; i =<arr.length i++ ){
+    if (numbers[i] === target){
+      return i
+    } else {
+      return none;
+    }
+  }
+}
+const result = linearSearch(numbers,6);
+console.log(result);
