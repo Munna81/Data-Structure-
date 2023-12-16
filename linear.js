87e291f6fdd903f2f1831d91f65e528770a47202
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-16 23:57:09 +0600

    Update linear.js

--- linear.js
+++ linear.js
@@ -1,7 +1,7 @@
 let numbers = [1,2,3,4,5,6,7,8,9];
 
 function linearSearch (arr,target) {
-  for(i=0; i =<arr.length i++ ){
+  for(let i = 0; i<arr.length; i++ ){
     if (numbers[i] === target){
       return i
     } else {
